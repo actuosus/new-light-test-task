3fafe90e6523d9902aaa0f26cea6f275d68edf99
Arthur Chafonov <actuosus@gmail.com>
2026-01-25 19:00:25 +0300

    feat: enhance swagger documentation with dynamic external repository links

--- src/shared/interface/http/swaggerPlugin.ts
+++ src/shared/interface/http/swaggerPlugin.ts
@@ -1,6 +1,65 @@
 import swagger from "@elysiajs/swagger";
 import packageJson from "../../../../package.json";
 
+const externalDocs = (repository: string | { url: string }) => {
+  if (typeof repository === "string") {
+    if (repository.startsWith("github:")) {
+      const repoPath = repository.replace("github:", "");
+      return {
+        url: `https://github.com/${repoPath}`,
+        description: "GitHub repository",
+      };
+    }
+    if (repository.startsWith("gitlab:")) {
+      const repoPath = repository.replace("gitlab:", "");
+      return {
+        url: `https://gitlab.com/${repoPath}`,
+        description: "GitLab repository",
+      };
+    }
+    if (repository.startsWith("bitbucket:")) {
+      const repoPath = repository.replace("bitbucket:", "");
+      return {
+        url: `https://bitbucket.org/${repoPath}`,
+        description: "Bitbucket repository",
+      };
+    }
+    return {
+      url: repository,
+      description: "GitHub repository",
+    };
+  } else {
+    return {
+      url: repository.url,
+      description: "Repository",
+    };
+  }
+};
+
+const licenseInfo = (license: string) => {
+  switch (license.toLowerCase()) {
+    case "mit":
+      return {
+        name: "MIT License",
+        url: "https://opensource.org/license/mit/",
+      };
+    case "apache-2.0":
+      return {
+        name: "Apache License 2.0",
+        url: "https://www.apache.org/licenses/LICENSE-2.0",
+      };
+    case "gpl-3.0":
+      return {
+        name: "GNU General Public License v3.0",
+        url: "https://www.gnu.org/licenses/gpl-3.0.en.html",
+      };
+    default:
+      return {
+        name: license,
+      };
+  }
+};
+
 export const swaggerPlugin = swagger({
   exclude: ["/swagger"],
   autoDarkMode: true,
@@ -9,19 +68,13 @@ export const swaggerPlugin = swagger({
       title: "New Light Task Service",
       description: packageJson.description,
       version: packageJson.version,
-      license: {
-        name: packageJson.license,
-        url: "https://opensource.org/license/mit/",
-      },
+      license: licenseInfo(packageJson.license || ""),
       contact: {
         name: packageJson.author.name,
         url: packageJson.author.url,
       },
     },
-    externalDocs: {
-      url: packageJson.repository.url,
-      description: "GitHub repository",
-    },
+    externalDocs: externalDocs(packageJson.repository),
     tags: [
       {
         name: "Tasks",
